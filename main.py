bd7365a3acbe011cf0d09839adbf5fca5fe10b80
vikas <vikassonisvnit@gmail.com>
2021-03-16 03:40:22 +0000

    first

---
+++ main.py
@@ -0,0 +1,188 @@
+
+from datetime import datetime
+import logging
+import os
+
+from flask import Flask, redirect, render_template, request
+
+from google.cloud import datastore
+from google.cloud import storage
+from google.cloud import vision
+from google.cloud import language_v1
+
+
+CLOUD_STORAGE_BUCKET = os.environ.get("CLOUD_STORAGE_BUCKET")
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def homepage():
+    # Create a Cloud Datastore client.
+    datastore_client = datastore.Client()
+
+    # Use the Cloud Datastore client to fetch information from Datastore about
+    # each photo.
+    query = datastore_client.query(kind="Faces")
+    image_entities = list(query.fetch())
+
+    query = datastore_client.query(kind="sentiment")
+    text_entities = list(query.fetch())
+
+    # Return a Jinja2 HTML template and pass in image_entities as a parameter.
+    return render_template("homepage.html", image_entities=image_entities, text_entities=text_entities)
+
+
+@app.route("/upload_photo", methods=["GET", "POST"])
+def upload_photo():
+    photo = request.files["file"]
+
+    # Create a Cloud Storage client.
+    storage_client = storage.Client()
+
+    # Get the bucket that the file will be uploaded to.
+    bucket = storage_client.get_bucket(CLOUD_STORAGE_BUCKET)
+
+    # Create a new blob and upload the file's content.
+    blob = bucket.blob(photo.filename)
+    blob.upload_from_string(photo.read(), content_type=photo.content_type)
+
+    # Make the blob publicly viewable.
+    blob.make_public()
+
+    # Create a Cloud Vision client.
+    vision_client = vision.ImageAnnotatorClient()
+
+    # Use the Cloud Vision client to detect a face for our image.
+    source_uri = "gs://{}/{}".format(CLOUD_STORAGE_BUCKET, blob.name)
+    image = vision.Image(source=vision.ImageSource(gcs_image_uri=source_uri))
+    faces = vision_client.face_detection(image=image).face_annotations
+
+    # If a face is detected, save to Datastore the likelihood that the face
+    # displays 'joy,' as determined by Google's Machine Learning algorithm.
+    if len(faces) > 0:
+        face = faces[0]
+
+        # Convert the likelihood string.
+        likelihoods = [
+            "Unknown",
+            "Very Unlikely",
+            "Unlikely",
+            "Possible",
+            "Likely",
+            "Very Likely",
+        ]
+        face_joy = likelihoods[face.joy_likelihood]
+    else:
+        face_joy = "Unknown"
+
+    # Create a Cloud Datastore client.
+    datastore_client = datastore.Client()
+
+    # Fetch the current date / time.
+    current_datetime = datetime.now()
+
+    # The kind for the new entity.
+    kind = "Faces"
+
+    # The name/ID for the new entity.
+    name = blob.name
+
+    # Create the Cloud Datastore key for the new entity.
+    key = datastore_client.key(kind, name)
+
+    # Construct the new entity using the key. Set dictionary values for entity
+    # keys blob_name, storage_public_url, timestamp, and joy.
+    entity = datastore.Entity(key)
+    entity["blob_name"] = blob.name
+    entity["image_public_url"] = blob.public_url
+    entity["timestamp"] = current_datetime
+    entity["joy"] = face_joy
+
+    # Save the new entity to Datastore.
+    datastore_client.put(entity)
+
+    # Redirect to the home page.
+    return redirect("/")
+
+
+@app.route("/upload_text", methods=["GET", "POST"])
+def upload_text():
+    photo = request.files["file"]
+
+    # Create a Cloud Storage client.
+    storage_client = storage.Client()
+
+    # Get the bucket that the file will be uploaded to.
+    bucket = storage_client.get_bucket(CLOUD_STORAGE_BUCKET)
+
+    # Create a new blob and upload the file's content.
+    blob = bucket.blob(photo.filename)
+    blob.upload_from_string(photo.read(), content_type=photo.content_type)
+
+    # Make the blob publicly viewable.
+    blob.make_public()
+
+    # Create a Cloud Vision client.
+    #vision_client = vision.ImageAnnotatorClient()
+    nlp_client = language_v1.LanguageServiceClient()
+    type_ = language_v1.Document.Type.PLAIN_TEXT
+    language = "en"
+    encoding_type = language_v1.EncodingType.UTF8
+
+    # Use the Cloud Vision client to detect a face for our image.
+    source_uri = "gs://{}/{}".format(CLOUD_STORAGE_BUCKET, blob.name)
+    document = {"gcs_content_uri": source_uri, "type_": type_, "language": language}
+
+    response = nlp_client.analyze_sentiment(request={'document': document, 'encoding_type': encoding_type})
+    print(u"Document sentiment score: {}".format(response.document_sentiment.score))
+
+    # Create a Cloud Datastore client.
+    datastore_client = datastore.Client()
+
+    # Fetch the current date / time.
+    current_datetime = datetime.now()
+
+    # The kind for the new entity.
+    kind = "sentiment"
+
+    # The name/ID for the new entity.
+    name = blob.name
+
+    # Create the Cloud Datastore key for the new entity.
+    key = datastore_client.key(kind, name)
+
+    # Construct the new entity using the key. Set dictionary values for entity
+    # keys blob_name, storage_public_url, timestamp, and joy.
+    entity = datastore.Entity(key)
+    entity["blob_name"] = blob.name
+    entity["image_public_url"] = blob.public_url
+    entity["timestamp"] = current_datetime
+    entity["sentiment"] = response.document_sentiment.score
+
+    # Save the new entity to Datastore.
+    datastore_client.put(entity)
+
+    # Redirect to the home page.
+    return redirect("/")
+
+
+@app.errorhandler(500)
+def server_error(e):
+    logging.exception("An error occurred during a request.")
+    return (
+        """
+    An internal error occurred: <pre>{}</pre>
+    See logs for full stacktrace.
+    """.format(
+            e
+        ),
+        500,
+    )
+
+
+if __name__ == "__main__":
+    # This is used when running locally. Gunicorn is used to run the
+    # application on Google App Engine. See entrypoint in app.yaml.
+    app.run(host="127.0.0.1", port=8080, debug=True)
